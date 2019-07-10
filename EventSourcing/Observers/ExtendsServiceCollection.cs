7b87eaca3c0444832b38ab34775c432d09de6a26
James Woodley <james@lightestnight.com>
2019-07-10 17:44:21 +0100

    Move projection writing to the observers and simplify Observer construction

--- EventSourcing/Observers/ExtendsServiceCollection.cs
+++ EventSourcing/Observers/ExtendsServiceCollection.cs
@@ -1,12 +1,9 @@
-using System;
 using System.Collections.Generic;
 using System.Linq;
 using System.Reflection;
 using System.Threading.Tasks;
 using LightestNight.System.EventSourcing.Observers.Rules;
 using LightestNight.System.EventSourcing.Observers.Rules.Mappers;
-using LightestNight.System.EventSourcing.Projections;
-using LightestNight.System.ServiceResolution;
 using LightestNight.System.Utilities.Extensions;
 using Microsoft.Extensions.DependencyInjection;
 using Microsoft.Extensions.Hosting;
@@ -26,8 +23,6 @@ namespace LightestNight.System.EventSourcing.Observers
 
             Parallel.ForEach(assemblies, assembly =>
             {
-                Parallel.ForEach(assembly.GetInstancesOfInterface<IProjectionWriterFactory>(), factoryType => { services.AddTransient(typeof(IProjectionWriterFactory), factoryType); });
-                
                 WireEventRuleMappers(assembly, services);
                 WireEventRules(assembly, services);
                 WireObserverOrchestrators(assembly, services);
@@ -80,45 +75,7 @@ namespace LightestNight.System.EventSourcing.Observers
         private static void WireEventObservers(Assembly assembly, IServiceCollection services)
         {
             var eventObservers = assembly.GetInstancesOfInterface<IEventObserver>();
-            var serviceProvider = services.BuildServiceProvider();
-            
-            var registeredProjectionWriterFactories = serviceProvider.GetServices<IProjectionWriterFactory>().ToArray();
-            var serviceFactory = serviceProvider.GetService<ServiceFactory>() ?? Activator.CreateInstance;
-            
-            Parallel.ForEach(eventObservers, eventObserver =>
-            {
-                // Rule: Event Observers can only have either a parameter-less constructor, or one taking an IProjectionWriter instance
-                var constructors = eventObserver.GetConstructors()
-                    .Where(c => c.GetParameters().Any(p => p.ParameterType.GetGenericTypeDefinition() == typeof(IProjectionWriter<,>)) || !c.GetParameters().Any())
-                    .ToArray();
-                
-                if (constructors.Length != 1)
-                    throw new InvalidOperationException("An instance of IEventObserver can only have a parameter-less constructor, or a constructor with only one IProjectionWriter implementation.");
-
-                var constructorParams = constructors.Single().GetParameters();
-                if (constructorParams.IsNullOrEmpty())
-                {
-                    services.AddTransient(typeof(IEventObserver), eventObserver);
-                    return;
-                }
-
-                if (registeredProjectionWriterFactories.IsNullOrEmpty())
-                    return;
-
-                var projectionWriterGenericArgs = constructorParams.Single().ParameterType.GetGenericArguments();
-                var idType = projectionWriterGenericArgs[0];
-                var readModelType = projectionWriterGenericArgs[1];
-                Parallel.ForEach(registeredProjectionWriterFactories, projectionWriterFactory =>
-                {
-                    var factoryMethod = projectionWriterFactory.GetType().GetMethod(nameof(IProjectionWriterFactory.GetProjectionWriter))
-                        ?.MakeGenericMethod(idType, readModelType);
-                    if (factoryMethod == null)
-                        return;
-
-                    var projectionWriterInstance = factoryMethod.Invoke(projectionWriterFactory, null);
-                    services.AddTransient(_ => serviceFactory(eventObserver, projectionWriterInstance) as IEventObserver);
-                });
-            });
+            Parallel.ForEach(eventObservers, eventObserver => services.AddTransient(typeof(IEventObserver), eventObserver));
 
             WireObserverCache(services);
         }
