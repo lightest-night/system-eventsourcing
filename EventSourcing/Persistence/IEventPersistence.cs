b95f121a270b42764691bebc95b0880df857ec8b
James Woodley <james@lightestnight.com>
2020-04-22 16:59:28 +0100

    Add Cancellation Tokens to IEventPersistence

--- EventSourcing/Persistence/IEventPersistence.cs
+++ EventSourcing/Persistence/IEventPersistence.cs
@@ -1,4 +1,5 @@
 using System;
+using System.Threading;
 using System.Threading.Tasks;
 using LightestNight.System.EventSourcing.Domain;
 
@@ -10,15 +11,17 @@ namespace LightestNight.System.EventSourcing.Persistence
         /// Gets an <see cref="IEventSourceAggregate" /> object from the persistence store by it's identifier
         /// </summary>
         /// <param name="id">The Globally Unique Identifier of the <see cref="IEventSourceAggregate" /> object</param>
+        /// <param name="cancellationToken">Any <see cref="CancellationToken" /> needed to marshal the operation</param>
         /// <typeparam name="T">The type of the <see cref="IEventSourceAggregate" /> to retrieve</typeparam>
         /// <returns>A populated instance of <see cref="IEventSourceAggregate" /></returns>
-        Task<T> GetById<T>(Guid id)
+        Task<T> GetById<T>(Guid id, CancellationToken cancellationToken = default)
             where T : class, IEventSourceAggregate;
 
         /// <summary>
         /// Writes the given <see cref="IEventSourceAggregate" /> object to the persistence store
         /// </summary>
         /// <param name="aggregate">The <see cref="IEventSourceAggregate" /> to write</param>
-        Task Save(IEventSourceAggregate aggregate);
+        /// <param name="cancellationToken">Any <see cref="CancellationToken" /> needed to marshal the operation</param>
+        Task Save(IEventSourceAggregate aggregate, CancellationToken cancellationToken = default);
     }
 }
