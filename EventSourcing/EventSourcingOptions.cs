087c23b4a8cb5d94a72e6a6ae9dee881b9ffe219
James Woodley <james@lightestnight.com>
2020-04-25 12:37:40 +0100

    Add EventSourcingOptions

---
+++ EventSourcing/EventSourcingOptions.cs
@@ -0,0 +1,10 @@
+﻿namespace LightestNight.System.EventSourcing
+{
+    public class EventSourcingOptions
+    {
+        /// <summary>
+        /// The amount of times a subscription will attempt to reconnect before reporting failure
+        /// </summary>
+        public int SubscriptionRetryCount { get; set; } = 5;
+    }
+}
