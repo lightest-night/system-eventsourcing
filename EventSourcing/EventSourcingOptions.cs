d077737a7294501e6520f0934064ac9f92472db5
James Woodley <james@lightestnight.com>
2020-04-27 18:49:23 +0100

    Add MaxReadStreamForward to EventSourcingOptions

--- EventSourcing/EventSourcingOptions.cs
+++ EventSourcing/EventSourcingOptions.cs
@@ -6,5 +6,10 @@
         /// The amount of times a subscription will attempt to reconnect before reporting failure
         /// </summary>
         public int SubscriptionRetryCount { get; set; } = 5;
+
+        /// <summary>
+        /// When reading an Event Store Stream forward, the maximum amount of events to retrieve per page
+        /// </summary>
+        public int MaxReadStreamForward { get; set; } = 500;
     }
 }
