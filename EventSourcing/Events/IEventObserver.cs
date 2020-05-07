15e113498018f4cd7ebba01684fd9f538a15e0db
James Woodley <james@lightestnight.com>
2020-05-07 19:15:58 +0100

    Add position & version to the IEventObserver interface

--- EventSourcing/Events/IEventObserver.cs
+++ EventSourcing/Events/IEventObserver.cs
@@ -9,9 +9,11 @@ namespace LightestNight.System.EventSourcing.Events
         /// When an Event is received this function is the one that is invoked to process the event.
         /// </summary>
         /// <param name="event">The event that is being observed</param>
+        /// <param name="position">The position in the stream the received event occurred</param>
+        /// <param name="version">The version of the received event</param> 
         /// <param name="cancellationToken">Any <see cref="CancellationToken" /> in use to marshall the request</param>
         /// <typeparam name="TEvent">The type of the event being observed</typeparam>
-        Task EventReceived<TEvent>(TEvent @event, CancellationToken cancellationToken);
+        Task EventReceived<TEvent>(TEvent @event, long? position = default, int? version = default, CancellationToken cancellationToken = default);
 
         /// <summary>
         /// Denotes whether the observer is active, if not, any events received will be ignored
