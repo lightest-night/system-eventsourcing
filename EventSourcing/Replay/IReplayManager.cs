7d5311f69dc0a49acbf251907b78ab74b3211312
James Woodley <james@lightestnight.com>
2020-05-09 19:11:19 +0100

    Add IReplayManager enforcing a stream replay from a particular checkpoint

---
+++ EventSourcing/Replay/IReplayManager.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Threading;
+using System.Threading.Tasks;
+
+namespace LightestNight.System.EventSourcing.Replay
+{
+    public interface IReplayManager
+    {
+        /// <summary>
+        /// Replays a stream of events firing the <paramref name="eventReceived" /> function for each
+        /// </summary>
+        /// <param name="streamId">The identifier of the stream to catch up</param>
+        /// <param name="fromCheckpoint">The current checkpoint</param>
+        /// <param name="eventReceived">Function to invoke when an event is received</param>
+        /// <param name="cancellationToken">Any <see cref="CancellationToken" /> used to marshall the operation</param>
+        /// <returns>The value of where in the stream the new checkpoint is</returns>
+        Task<int> ReplayProjectionFrom(string streamId, int fromCheckpoint, Func<object, CancellationToken, Task> eventReceived, CancellationToken cancellationToken = default);
+    }
+}
--- EventSourcing/Subscriptions/IPersistentSubscriptionManager.cs
+++ EventSourcing/Subscriptions/IPersistentSubscriptionManager.cs
@@ -24,21 +24,16 @@ namespace LightestNight.System.EventSourcing.Subscriptions
         Task CloseSubscription(Guid subscriptionId, CancellationToken cancellationToken = default);
 
         /// <summary>
-        /// Catches a subscription up to the current point in the stream 
-        /// </summary>
-        /// <param name="streamName">The name of the stream to catch up</param>
-        /// <param name="checkpoint">The current checkpoint</param>
-        /// <param name="eventReceived">Function to invoke when an event is received</param>
-        /// <param name="cancellationToken">Any <see cref="CancellationToken" /> used to marshall the operation</param>
-        /// <returns>The new value for <paramref name="checkpoint" /></returns>
-        Task<int> CatchSubscriptionUp(string streamName, int checkpoint, Func<object, CancellationToken, Task> eventReceived, CancellationToken cancellationToken = default);
-        
-        /// <summary>
         /// Saves the given checkpoint under the given name
         /// </summary>
         /// <param name="checkpoint">The checkpoint to save</param>
         /// <param name="checkpointName">The name of the checkpoint</param>
         /// <param name="cancellationToken">Any <see cref="CancellationToken" /> used to marshall the operation</param>
         Task SaveCheckpoint(int checkpoint, [CallerMemberName] string? checkpointName = default, CancellationToken cancellationToken = default);
+        
+        /// <summary>
+        /// The current global checkpoint position
+        /// </summary>
+        int GlobalCheckpoint { get; }
     }
 }
