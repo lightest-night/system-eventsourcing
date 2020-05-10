4a1b8565f93ab6c4652ed810e4189a8df71c2c9e
James Woodley <james@lightestnight.com>
2020-05-10 17:25:47 +0100

    Add SaveGlobalCheckpoint to IPersistentSubscriptionManager

--- EventSourcing/Subscriptions/IPersistentSubscriptionManager.cs
+++ EventSourcing/Subscriptions/IPersistentSubscriptionManager.cs
@@ -30,7 +30,13 @@ namespace LightestNight.System.EventSourcing.Subscriptions
         /// <param name="checkpointName">The name of the checkpoint</param>
         /// <param name="cancellationToken">Any <see cref="CancellationToken" /> used to marshall the operation</param>
         Task SaveCheckpoint(int checkpoint, [CallerMemberName] string? checkpointName = default, CancellationToken cancellationToken = default);
-        
+
+        /// <summary>
+        /// Saves the global checkpoint
+        /// </summary>
+        /// <param name="checkpoint">The global checkpoint to set</param>
+        Task SaveGlobalCheckpoint(int checkpoint);
+
         /// <summary>
         /// The current global checkpoint position
         /// </summary>
