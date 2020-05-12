b03055e1aacc958cc1a6ee42eda774bc67fd3d32
James Woodley <james@lightestnight.com>
2020-05-12 12:48:53 +0100

    Add ReplayProjectionFrom for the global stream

--- EventSourcing/Replay/IReplayManager.cs
+++ EventSourcing/Replay/IReplayManager.cs
@@ -1,4 +1,5 @@
 ﻿using System;
+using System.Runtime.CompilerServices;
 using System.Threading;
 using System.Threading.Tasks;
 
@@ -6,6 +7,16 @@ namespace LightestNight.System.EventSourcing.Replay
 {
     public interface IReplayManager
     {
+        /// <summary>
+        /// Replays the global stream firing the <paramref name="eventReceived" /> function for each
+        /// </summary>
+        /// <param name="fromCheckpoint">The current checkpoint</param>
+        /// <param name="eventReceived">Function to invoke when an event is received</param>
+        /// <param name="projectionName">The name of the projection that is being replayed</param>
+        /// <param name="cancellationToken">Any <see cref="CancellationToken" /> used to marshall the operation</param>
+        /// <returns>The value of where in the stream the new checkpoint is</returns>
+        Task<long> ReplayProjectionFrom(long? fromCheckpoint, Func<object, CancellationToken, Task> eventReceived, [CallerMemberName] string? projectionName = default, CancellationToken cancellationToken = default);
+        
         /// <summary>
         /// Replays a stream of events firing the <paramref name="eventReceived" /> function for each
         /// </summary>
