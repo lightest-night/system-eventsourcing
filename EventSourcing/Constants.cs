70159af7ac185c813d1e492df4572d94eccb1649
James Woodley <james@lightestnight.com>
2020-05-18 16:40:49 +0100

    Remove IPersistentSubscriptionManager and Category constant

--- EventSourcing/Constants.cs
+++ EventSourcing/Constants.cs
@@ -7,6 +7,5 @@
         public const string CheckpointPrefix = "checkpoint";
         public const string CheckpointMessageType = "checkpoint";
         public const string GlobalCheckpointId = "global";
-        public static readonly string CategoryPrefix = $"{SystemStreamPrefix}ce-";
     }
 }
