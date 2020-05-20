55d5d07a16e6b9d9db546ab390645c8c18d24371
James Woodley <james@lightestnight.com>
2020-05-20 09:39:27 +0100

    Change ICheckpointManager to delegates

---
+++ EventSourcing/Checkpoints/CheckpointDelegates.cs
@@ -0,0 +1,19 @@
+﻿using System.Threading;
+using System.Threading.Tasks;
+
+namespace LightestNight.System.EventSourcing.Checkpoints
+{
+    /// <summary>
+    /// Gets the global checkpoint
+    /// </summary>
+    /// <param name="cancellationToken">Any <see cref="CancellationToken" /> used to marshall the operation</param>
+    /// <returns>The current global checkpoint</returns>
+    public delegate Task<long?> GetGlobalCheckpoint(CancellationToken cancellationToken = default);
+
+    /// <summary>
+    /// Sets the global checkpoint value to the given value
+    /// </summary>
+    /// <param name="checkpoint">The checkpoint to set</param>
+    /// <param name="cancellationToken">Any <see cref="CancellationToken" /> used to marshall the operation</param>
+    public delegate Task SetGlobalCheckpoint(long? checkpoint, CancellationToken cancellationToken = default);
+}
