b9dc6740570acef30abb61c7eafd541b6ada71e8
James Woodley <james@lightestnight.com>
2020-05-21 18:30:15 +0100

    Add Cancellation support to ICheckpointManager

--- EventSourcing/Checkpoints/ICheckpointManager.cs
+++ EventSourcing/Checkpoints/ICheckpointManager.cs
@@ -1,4 +1,5 @@
 ﻿using System.Runtime.CompilerServices;
+using System.Threading;
 using System.Threading.Tasks;
 
 namespace LightestNight.System.EventSourcing.Checkpoints
@@ -10,15 +11,17 @@ namespace LightestNight.System.EventSourcing.Checkpoints
         /// </summary>
         /// <param name="checkpoint">The checkpoint to set</param>
         /// <param name="checkpointName">If set, a name to assign the checkpoint (defaults to the calling member's name)</param>
+        /// <param name="cancellationToken">Any <see cref="CancellationToken" /> used to marshall the operation</param>
         /// <typeparam name="TCheckpoint">The Type of the checkpoint to set</typeparam>
-        Task SetCheckpoint<TCheckpoint>(TCheckpoint checkpoint, [CallerMemberName] string? checkpointName = default);
+        Task SetCheckpoint<TCheckpoint>(TCheckpoint checkpoint, [CallerMemberName] string? checkpointName = default, CancellationToken cancellationToken = default);
 
         /// <summary>
         /// Gets the checkpoint with the given name
         /// </summary>
         /// <param name="checkpointName">The checkpoint name (defaults to the calling member's name)</param>
+        /// <param name="cancellationToken">Any <see cref="CancellationToken" /> used to marshall the operation</param>
         /// <typeparam name="TCheckpoint">The Type of the checkpoint to get</typeparam>
         /// <returns>The checkpoint</returns>
-        Task<TCheckpoint> GetCheckpoint<TCheckpoint>([CallerMemberName] string? checkpointName = default);
+        Task<TCheckpoint> GetCheckpoint<TCheckpoint>([CallerMemberName] string? checkpointName = default, CancellationToken cancellationToken = default);
     }
 }
