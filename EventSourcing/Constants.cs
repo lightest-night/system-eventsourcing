df67b2520789695f9c986771d9e60002af78938e
James Woodley <james@lightestnight.com>
2020-05-27 11:01:52 +0100

    Add TimestampKey Constant

--- EventSourcing/Constants.cs
+++ EventSourcing/Constants.cs
@@ -7,5 +7,6 @@
         public const string CheckpointPrefix = "checkpoint";
         public const string CheckpointMessageType = "checkpoint";
         public const string GlobalCheckpointId = "global";
+        public const string TimestampKey = "timestamp";
     }
 }
