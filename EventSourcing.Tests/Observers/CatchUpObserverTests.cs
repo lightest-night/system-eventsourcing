0e5970792f5afbfef7ed0d432472c41f9d97cd9d
James Woodley <james@lightestnight.com>
2020-08-18 11:06:24 +0100

    * Observers
      * Add CatchUp observer to act as a base for when the observer should always be current

---
+++ EventSourcing.Tests/Observers/CatchUpObserverTests.cs
@@ -0,0 +1,74 @@
+﻿using System.Threading;
+using System.Threading.Tasks;
+using LightestNight.System.EventSourcing.Checkpoints;
+using LightestNight.System.EventSourcing.Events;
+using LightestNight.System.EventSourcing.Observers;
+using LightestNight.System.EventSourcing.Replay;
+using Microsoft.Extensions.Logging;
+using Microsoft.Extensions.Logging.Abstractions;
+using Moq;
+using Shouldly;
+using Xunit;
+
+namespace LightestNight.System.EventSourcing.Tests.Observers
+{
+    internal class TestCatchUpObserver : CatchUpObserver
+    {
+        public TestCatchUpObserver(string checkpointName, ICheckpointManager checkpointManager,
+            IReplayManager replayManager, ILogger logger, GetGlobalCheckpoint getGlobalCheckpoint) : base(
+            checkpointName, checkpointManager, replayManager, logger, getGlobalCheckpoint)
+        {
+        }
+
+        public override Task EventReceived(object @event, long? position = null, int? version = null,
+            CancellationToken cancellationToken = default)
+            => Task.CompletedTask;
+    }
+    
+    public class CatchUpObserverTests
+    {
+        private const long Checkpoint = 100;
+        private const string CheckpointName = "Test Checkpoint";
+        private readonly Mock<ICheckpointManager> _checkpointManagerMock = new Mock<ICheckpointManager>();
+        private readonly Mock<IReplayManager> _replayManagerMock = new Mock<IReplayManager>();
+
+        public CatchUpObserverTests()
+        {
+            _checkpointManagerMock
+                .Setup(checkpointManager => checkpointManager.GetCheckpoint(CheckpointName, CancellationToken.None))
+                .ReturnsAsync(Checkpoint);
+        }
+        
+        [Fact]
+        public async Task ShouldSetActiveToTrueIfCheckpointsAreEqual()
+        {
+            // Arrange
+            _replayManagerMock
+                .Setup(replayManager =>
+                    replayManager.ReplayProjectionFrom(null, It.IsAny<EventReceived>(), typeof(TestCatchUpObserver).FullName,
+                        CancellationToken.None)).ReturnsAsync(Checkpoint);
+            
+            // Act
+            var sut = new TestCatchUpObserver(CheckpointName, _checkpointManagerMock.Object,
+                _replayManagerMock.Object, NullLogger.Instance,
+                cancellationToken => Task.FromResult((long?) Checkpoint));
+            
+            // Allow a pause due to the asynchronous nature of setting checkpoints
+            await Task.Delay(50);
+            
+            // Assert
+            sut.IsActive.ShouldBeTrue();
+        }
+
+        [Fact]
+        public void ShouldSetActiveToFalseIfCheckpointsAreNotEqual()
+        {
+            // Act
+            var sut = new TestCatchUpObserver(CheckpointName, _checkpointManagerMock.Object, _replayManagerMock.Object,
+                NullLogger.Instance, cancellationToken => Task.FromResult((long?) Checkpoint));
+            
+           // Assert
+           sut.IsActive.ShouldBeFalse();
+        }
+    }
+}
---
+++ EventSourcing/Observers/CatchUpObserver.cs
@@ -0,0 +1,87 @@
+﻿using System;
+using System.ComponentModel;
+using System.Diagnostics;
+using System.Runtime.CompilerServices;
+using System.Threading;
+using System.Threading.Tasks;
+using LightestNight.System.EventSourcing.Checkpoints;
+using LightestNight.System.EventSourcing.Events;
+using LightestNight.System.EventSourcing.Replay;
+using Microsoft.Extensions.Logging;
+
+namespace LightestNight.System.EventSourcing.Observers
+{
+    public abstract class CatchUpObserver : IEventObserver
+    {
+        private readonly string _checkpointName;
+        private readonly ICheckpointManager _checkpointManager;
+        private readonly IReplayManager _replayManager;
+        private readonly ILogger _logger;
+
+        private static long? _checkpoint;
+
+        private bool _isActive;
+
+        public event PropertyChangedEventHandler? PropertyChanged;
+
+        public bool IsActive
+        {
+            get => _isActive;
+            private set
+            {
+                if (value == _isActive)
+                    return;
+
+                _isActive = value;
+                NotifyPropertyChanged();
+            }
+        }
+
+        protected CatchUpObserver(string checkpointName, ICheckpointManager checkpointManager,
+            IReplayManager replayManager, ILogger logger, GetGlobalCheckpoint getGlobalCheckpoint)
+        {
+            _checkpointName = checkpointName ?? throw new ArgumentNullException(nameof(checkpointName));
+            _checkpointManager = checkpointManager ?? throw new ArgumentNullException(nameof(checkpointManager));
+            _replayManager = replayManager ?? throw new ArgumentNullException(nameof(replayManager));
+            _logger = logger ?? throw new ArgumentNullException(nameof(logger));
+
+            var globalCheckpoint = (getGlobalCheckpoint ?? throw new ArgumentNullException(nameof(getGlobalCheckpoint)))().Result;
+            if (globalCheckpoint == _checkpoint)
+                IsActive = true;
+            else
+                Task.Run(async () => await CatchUp().ConfigureAwait(false));
+        }
+
+        public abstract Task EventReceived(object @event, long? position = null, int? version = null,
+            CancellationToken cancellationToken = default);
+
+        private Task SetCheckpoint(long? checkpoint, CancellationToken cancellationToken = default)
+        {
+            _checkpoint = checkpoint;
+            return _checkpoint.HasValue
+                ? _checkpointManager.SetCheckpoint(_checkpointName, _checkpoint.Value, cancellationToken)
+                : _checkpointManager.ClearCheckpoint(_checkpointName, cancellationToken);
+        }
+
+        private void NotifyPropertyChanged([CallerMemberName] string propertyName = "")
+        {
+            PropertyChanged?.Invoke(this, new PropertyChangedEventArgs(propertyName));
+        }
+
+        private async Task CatchUp(CancellationToken cancellationToken = default)
+        {
+            var projectionName = GetType().FullName;
+            var stopwatch = Stopwatch.StartNew();
+
+            var currentCheckpoint = await _replayManager
+                .ReplayProjectionFrom(_checkpoint, EventReceived, projectionName, cancellationToken)
+                .ConfigureAwait(false);
+
+            await SetCheckpoint(currentCheckpoint, cancellationToken).ConfigureAwait(false);
+            IsActive = true;
+
+            stopwatch.Stop();
+            _logger.LogInformation($"{projectionName} caught up in {stopwatch.ElapsedMilliseconds}ms");
+        }
+    }
+}
