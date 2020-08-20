02312c712d48e61abd582cac73251fe3596cbf97
James Woodley <james@lightestnight.com>
2020-08-20 11:56:50 +0100

    Add CheckpointIntervalKey to constants

--- EventSourcing/Constants.cs
+++ EventSourcing/Constants.cs
@@ -6,6 +6,7 @@
         public const string VersionKey = "version";
         public const string CheckpointPrefix = "checkpoint";
         public const string CheckpointMessageType = "checkpoint";
+        public const string CheckpointIntervalKey = "EventStore:Checkpoints:Interval";
         public const string GlobalCheckpointId = "global";
         public const string TimestampKey = "timestamp";
     }
