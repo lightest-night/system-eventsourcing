ad88a772d417b738d665f4942e4da173b25b04ec
James Woodley <james@lightestnight.com>
2020-08-25 21:32:10 +0100

    Beginnings of EventObserver static collection pattern

--- EventSourcing/Events/IEventObserver.cs
+++ EventSourcing/Events/IEventObserver.cs
@@ -1,11 +1,27 @@
-﻿using System.ComponentModel;
+﻿using System;
 using System.Threading;
 using System.Threading.Tasks;
 
 namespace LightestNight.System.EventSourcing.Events
 {
-    public interface IEventObserver : INotifyPropertyChanged
+    public interface IEventObserver : IAsyncDisposable
     {
+        /// <summary>
+        /// Denotes whether the observer is active, if not, any events received will be ignored
+        /// </summary>
+        bool IsActive { get; }
+        
+        /// <summary>
+        /// Denotes whether the observer has been disposed
+        /// </summary>
+        bool IsDisposed { get; set; }
+
+        /// <summary>
+        /// Called by our runtime to initialise the observer and get it ready to work
+        /// </summary>
+        /// <param name="cancellationToken">Any <see cref="CancellationToken" /> in use to marshall the request</param>
+        Task InitialiseObserver(CancellationToken cancellationToken = default);
+        
         /// <summary>
         /// When an Event is received this function is the one that is invoked to process the event.
         /// </summary>
@@ -14,10 +30,5 @@ namespace LightestNight.System.EventSourcing.Events
         /// <param name="version">The version of the received event</param> 
         /// <param name="cancellationToken">Any <see cref="CancellationToken" /> in use to marshall the request</param>
         Task EventReceived(object evt, long? position = default, int? version = default, CancellationToken cancellationToken = default);
-        
-        /// <summary>
-        /// Denotes whether the observer is active, if not, any events received will be ignored
-        /// </summary>
-        bool IsActive { get; }
     }
 }
---
+++ EventSourcing/Observers/ObserverCollection.cs
@@ -0,0 +1,43 @@
+﻿using System.Collections.Generic;
+using System.Threading;
+using System.Threading.Tasks;
+using LightestNight.System.EventSourcing.Events;
+
+namespace LightestNight.System.EventSourcing.Observers
+{
+    public static class ObserverCollection
+    {
+        private static readonly Dictionary<string, IEventObserver> Observers = new Dictionary<string, IEventObserver>();
+
+        /// <summary>
+        /// Registers an <see cref="IEventObserver" /> into the application
+        /// </summary>
+        /// <param name="observer">The observer to register</param>
+        /// <param name="cancellationToken">Any <see cref="CancellationToken" /> in use to marshall the request</param>
+        public static async Task RegisterObserver(IEventObserver observer, CancellationToken cancellationToken = default)
+        {
+            await observer.InitialiseObserver(cancellationToken).ConfigureAwait(false);
+            Observers.Add(observer.GetType().FullName, observer);
+        }
+
+        /// <summary>
+        /// Unregisters an <see cref="IEventObserver" /> from the application
+        /// </summary>
+        /// <param name="observer">The observer to unregister</param>
+        public static async Task UnregisterObserver(IEventObserver observer)
+        {
+            if (!observer.IsDisposed)
+            {
+                await observer.DisposeAsync();
+                observer.IsDisposed = true;
+            }
+        }
+
+        /// <summary>
+        /// Gets a collection of <see cref="IEventObserver" /> instances that have been registered into the application
+        /// </summary>
+        /// <returns>Collection of <see cref="IEventObserver" /> instances</returns>
+        public static IEnumerable<IEventObserver> GetEventObservers()
+            => Observers.Values;
+    }
+}
